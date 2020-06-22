5610fda4d8d3231c0d1d7521f135f53911820f92
Breon Green <bgreen@MacBook-Pro.attlocal.net>
2020-06-22 13:02:58 -0400

    moving params to vars

--- terraform/app/cs1/main.tf
+++ terraform/app/cs1/main.tf
@@ -1,5 +1,15 @@
+locals {
+
+  # resource group prefix
+  rsg_pfx         = "my-test-bgree13"
+
+  # regions
+  loc_eastus      = "eastus"
+  loc_westus      = "westus"
+
+}
 module "rsg-cs1" {
   source          = "../../shared-modules/rsg"
-  resource_group  = "my-test-bgree13"
-  locations       = ["eastus","westus"]
+  resource_group  = local.rsg_pfx
+  locations       = [local.loc_eastus,local.loc_westus]
 }
